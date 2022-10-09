bd460a521d4f2115350f81549e2b4fbc56d3ddb6
mseeam <mseeam99@gmail.com>
2022-10-09 02:47:28 -0500

    stack.cpp, includes stack.h and all important functions for stack

--- stack.cpp
+++ stack.cpp
@@ -1,11 +1,96 @@
-/*
- * Your comment header here
- * modify this file as needed to complete the assignment
- */
+/********************************************************************************
+ * File  :   stack.cpp                                                          *
+ * Author: Mohammad Seeam                                                       *
+ * Assignment 5: Stacks                                                         *
+ * This is a simple application to demonstrate struct and stack in C++.         *
+ * This header structure for all cpp and h files.                               *
+ * This file, stack.cpp, includes stack.h                                       *
+ ********************************************************************************/
 
-// each cpp includes ONLY it's header directly
 #include "stack.h"
+#include <string.h>
+#include <stdio.h>
+#include <iostream>
 
-/*
- * write all your stack methods here
- */
+Stack::Stack(int size) {
+
+    if(size<2){
+        size=10;
+    }
+
+    this->top = -1;
+    this->size=size;
+    this->stack = new Data*[size];
+}
+
+Stack::~Stack() {
+
+    //first delete the elements in the array
+    for(int element=top;element>-1; element--){
+        delete stack[element];
+    }
+    
+    top=-1;
+
+    delete[] stack;
+    stack=NULL;
+    size=0;
+}
+
+bool Stack::push(int id, string& information){
+    
+    bool check_push_success=false;
+
+    if(top<size-1 && id>=0 && information!=""){
+
+        Data* data= new Data;
+        data->id=id;
+        data->information=information;
+        stack[++top]=data;
+
+        check_push_success=true;
+
+    }
+
+    return check_push_success;
+}
+
+
+bool Stack::pop(Data *data) {
+
+    bool check_pop_success = false;
+
+    if(isEmpty()){
+        data->id=-1;
+        data->information="";
+    }else{
+        data->id=stack[top]->id;
+        data->information=stack[top]->information;
+        delete stack[top];
+        --top;
+        check_pop_success=true;
+
+    }
+    return check_pop_success;
+}
+
+bool Stack::peek(Data *data) {
+
+    bool check_peek_success = false;
+
+    if(isEmpty()){
+        data->id=-1;
+        data->information="";
+    }else{
+        data->id=stack[top]->id;
+        data->information=stack[top]->information;
+        check_peek_success=true;
+    }
+
+    return check_peek_success;
+}
+
+
+bool Stack::isEmpty() {
+    return top < 0;
+}
