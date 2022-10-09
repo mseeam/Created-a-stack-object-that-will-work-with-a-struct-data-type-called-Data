78226bf666ad466d9d2592c60d96f353ab48ef7c
mseeam <mseeam99@gmail.com>
2022-10-09 02:48:21 -0500

    stack.h has the class

--- stack.h
+++ stack.h
@@ -1,15 +1,14 @@
-/*
- * Your comment header here
- * modify this file as needed to complete the assignment
- */
+/********************************************************************************
+ * File  :   stack.h                                                            *
+ * Author: Mohammad Seeam                                                       *
+ * Assignment 5: Stacks                                                         *
+ * This is a simple application to demonstrate struct and stack in C++.         *
+ * This header structure for all cpp and h files.                               *
+ * This file, stack.h, includes only data.h                                     *
+ *******************************************************************************/
 
 #ifndef STACK_H
 #define STACK_H
-
-/*
- * there are no other includes here because they are not needed.
- * if you do need other things included, they would go here
- */
 
 #include "data.h"
 
@@ -17,24 +16,21 @@ class Stack {
 
 public:
 
-    /* 
-     * write all your public method prototypes here
-     * this includes you constructor and destructor
-     */
+    Stack(int); //constractor
+    ~Stack();   //destractor
+
+    //Declaring all the function
+    bool push(int,string&);
+    bool pop(Data*);
+    bool peek(Data*);
+    bool isEmpty();
     
 private:
 
-    /* 
-     * write all your private method prototypes here
-     */
-
-    // these are the only attributes you need.
-    // do not modify them or add any other attributes
     int top;
     int size;
-    Data **stack;
-    // notice stack is a pointer to a pointer. That means if you do
-    // a single array allocation, you will get an array of pointers
+    Data** stack;
+
 };
 
 #endif //STACK_H
