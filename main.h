3ab6b7d4c2c30e5d61e61bc043a1e4bbb4d05f5f
mseeam <mseeam99@gmail.com>
2022-10-09 02:46:43 -0500

    includes many library

--- main.h
+++ main.h
@@ -1,23 +1,28 @@
-/*
- * your header here
- * 
- */
+/********************************************************************************
+ * File  :   main.h                                                             *
+ * Author: Mohammad Seeam                                                       *
+ * Assignment 5: Stacks                                                         *
+ * This is a simple application to demonstrate struct and stack in C++.         *
+ * This header structure for all cpp and h files.                               *                                *
+ ********************************************************************************/
 
 #ifndef STACKS_MAIN_H
 #define STACKS_MAIN_H
 
-// you probably need all these,
-// but if you don't get rid of what you don't need
+#define MULTIPLIER 1.5 // used to test overflow and underflow
+#define NARROW .30 // percent of the stack for a narrow band
+#define RANDOM_MULTIPLIER 10
+#define CHOICES 6 // used to pick random operations
+#define MAX_INT 100000 // random ints to push, 1 - 100000
+
 #include <stdlib.h>   /* srand, rand */
 #include <time.h>     /* time */
 #include <iostream>   /* cout, endl */
 #include <string>     /* string */
 #include "functions.h"
-
-/*
- * additional directives here
- * 
- */
-
+#include "stack.h"
+#include <stdio.h>
+#include <ctype.h>
 
 #endif /* STACKS_MAIN_H */
+
